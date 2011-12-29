b58c79676a52f7b794a8c394ce51dff5214daa61
Ido Green <greenido@gmail.com>
2011-12-29 13:23:03 +0200

    add feeds from my blog and pipes that agg. some chrome news blogs

--- js/script.js
+++ js/script.js
@@ -5,21 +5,38 @@
 */
 
 $(document).ready(function() {
- // hides the slickbox as soon as the DOM is ready
+  $("#chromenews").hide();
   $("#more-head").hide();
- // shows the slickbox on clicking the noted link  
+  // shows the slickbox on clicking the noted link  
   $('#more-head-but').click(function() {
     $('#more-head').show('slow');
     $('#more-head-but').hide('slow');
     return false;
   });
- // hides the slickbox on clicking the noted link  
+  // hides the slickbox on clicking the noted link  
   $('#less-head-but').click(function() {
     $('#more-head').hide('fast');
     $('#more-head-but').show('fast');
     return false;
   });
  
+  $('#myblog-but').click(function() {
+    $('#chromenews').show('slow');
+    $('#myblog-but').hide('slow');
+    return false;
+  });
+  
+
+  $('#myblog').rssfeed('http://greenido.wordpress.com/feed/',
+  {
+    limit : 6
+  });
+  // a bunch of RSS feeds that coming from y! pipes (+caching)
+  $('#chromenews').rssfeed(
+    'http://pipes.yahoo.com/pipes/pipe.run?_id=b1503e8154a95936d28a1d5175017426&_render=rss',
+    {
+      limit : 10
+    });
 });
 
 
